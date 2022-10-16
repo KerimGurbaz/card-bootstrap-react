04a387f5a6c280ebb419253f841d1973e3894b95
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-16 10:47:02 +0200

    starter

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Language from "./Language";
+
+const Card = () => {
+  return (
+    <div>
+      <h1>CARD</h1>
+      <Language />
+    </div>
+  );
+};
+
+export default Card;
