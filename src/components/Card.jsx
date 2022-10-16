7933552ad5a1e4f3ebb828da5d3e8923d7518a04
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-16 12:55:23 +0200

    ol-li

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 import Language from "./Language";
+import { data } from "../helpers/data";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const Card = () => {
   return (
-    <div>
-      <h1>CARD</h1>
-      <Language />
-    </div>
+    <Container className="rounded-3 mt-4 p-4" style={{ background: "#f48b29" }}>
+      <h1 className="text-white my-2">Language</h1>
+      <Row className="g-3">
+        {data.map((lang, index) => {
+          return (
+            <Col sm={6} md={4} lg={3} key={index}>
+              <Language {...lang} />
+            </Col>
+          );
+        })}
+        <Col></Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
-const Language = () => {
-  return <div>Language</div>;
+const Language = ({ name, options, img }) => {
+  const [showImage, setShowImage] = useState(true);
+  console.log(showImage);
+
+  return (
+    <Container
+      className="p-4 rounded-5 h-100 lang-card"
+      style={{ background: "peachpuff" }}
+      onClick={() => setShowImage(!showImage)}
+      type="button"
+    >
+      {showImage && (
+        <Container>
+          <Image src={img} width="70%"></Image>
+          <h3 className="display-6 lang-logo">{name}</h3>
+        </Container>
+      )}
+      {!showImage && (
+        <ul className="h-100 d-flex flex-column justify-content-center">
+          {options.map((item, index) => {
+            return (
+              <li className="h5 text-start" key={index}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </Container>
+  );
 };
 
 export default Language;
